be1c61a80bd646595ecf317ef88529ffdd6b07e3
Ronald Stephens <rstephens@bluemorpho.cc>
2019-12-01 00:19:47 +0000

    Done.

--- conversions_test.rb
+++ conversions_test.rb
@@ -8,7 +8,7 @@ else
   puts "Test failed: got #{answer} instead"
 end
 
-puts "Given 1, it should return 28.3495"
+puts "Given 1, it should return 28.34951"
 answer = ounces_to_grams(1)
 if answer == 28.3495
   puts "Passed!"
